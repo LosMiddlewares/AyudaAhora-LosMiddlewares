ee56d4698a2d40d55da011ff8c115e9614932a2c
Fabian <fabianvillagra28@gmail.com>
2024-08-23 14:33:09 -0300

    fix: edicion de los componentes post

--- client/src/pages/Posts.jsx
+++ client/src/pages/Posts.jsx
@@ -1,83 +1,62 @@
 import './css/Posts.css'
 import imgPost from '../assets/userRandom.jpg';
 import {Post} from '../components/Post.jsx';
-
+import React, { useState, useEffect } from 'react';
 import Header from '../components/Header.jsx';
 import Footer from '../components/Footer.jsx';
+import { environments } from '../config/environments.js';
 
 function Posts() {
 
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const res = await fetch(`${environments.API_URL}/posts`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json'
+          }
+        });
+
+        if (!res.ok) {
+          const errorData = await res.json();
+          console.error(errorData.error || 'Error al obtener los posts');
+          return;
+        }
+
+        const data = await res.json();
+        setPosts(data);  // Guarda los posts en el estado
+      } catch (error) {
+        console.error('Error al obtener los posts:', error);
+      }
+    };
+
+    fetchPosts();  // Llama a la función para obtener los posts
+  }, []);
+
+
   return (
     <>
-      <div id='box-all-body'>
-        <Header/>
-          <div id='box-main-posts' className='animate__animated animate__zoomIn'>
-            <div className='box-post'>
-              <div className='box-img-userName'>
-                <img src={imgPost} alt="" />
-                <h4>@CaluBritez</h4>
-              </div>
-              <div className='box-text-post'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit. Sipsum dolor sit amet consectetur ........ </p>
-              </div>
-              <div className='box-button-post'>
-                <button>Ver más</button>
-              </div>
-            </div>
-            <div className='box-post'>
-              <div className='box-img-userName'>
-                <img src={imgPost} alt="" />
-                <h4>@Richard</h4>
-              </div>
-              <div className='box-text-post'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit. Sipsum dolor sit amet consectetur ........ </p>
-              </div>
-              <div className='box-button-post'>
-                <button>Ver más</button>
-              </div>
-            </div>
-            <div className='box-post'>
-              <div className='box-img-userName'>
-                <img src={imgPost} alt="" />
-                <h4>@AndyRodinck</h4>
-              </div>
-              <div className='box-text-post'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit. Sipsum dolor sit amet consectetur ........ </p>
-              </div>
-              <div className='box-button-post'>
-                <button>Ver más</button>
-              </div>
-            </div>
-            <div className='box-post'>
-              <div className='box-img-userName'>
-                <img src={imgPost} alt="" />
-                <h4>@RitoPompa</h4>
-              </div>
-              <div className='box-text-post'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit. Sipsum dolor sit amet consectetur ........ </p>
-              </div>
-              <div className='box-button-post'>
-                <button>Ver más</button>
-              </div>
-            </div>
-            <div className='box-post'>
-              <div className='box-img-userName'>
-                <img src={imgPost} alt="" />
-                <h4>@LaFurcade</h4>
-              </div>
-              <div className='box-text-post'>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Elit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit consectetur adipisicing elit. Sipsum dolor sit amet consectetur ........ </p>
-              </div>
-              <div className='box-button-post'>
-                <button>Ver más</button>
-              </div>
-            </div>
-            <Post userName="LuisSuarez" text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit. Sipsum dolor sit amet consectetur adipisicing elit." />
-          </div>
-        <Footer />
+    <div id='box-all-body'>
+      <Header />
+      <div id='box-main-posts' className='animate__animated animate__zoomIn'>
+        {posts.length > 0 ? (
+          posts.map(post => (
+            <Post
+              key={post.id}
+              title={post.title}
+              description={post.description}
+            />
+          ))
+        ) : (
+          <p>No hay posts disponibles</p>
+        )}
       </div>
-      
-    </>
+      <Footer />
+    </div>
+  </>
   )
 }
 export default Posts
