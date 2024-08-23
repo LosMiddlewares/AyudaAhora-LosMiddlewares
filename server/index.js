a457db4ba5d33a3d343ed0ee6ea6e2095f52ccc6
Silvana Farias <silvifarias17@gmail.com>
2024-08-23 00:16:27 -0300

    feat: configuración de multer para la subida de archivos

--- server/index.js
+++ server/index.js
@@ -3,7 +3,10 @@ import cors from 'cors';
 import morgan from 'morgan';
 import helmet from 'helmet';
 import path from 'path';
+import fs from 'node:fs';
 import 'dotenv/config';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
 
 import { connectDataBase } from './src/db/connection.js';
 import { relations } from './src/db/relations.js';
@@ -13,10 +16,16 @@ const app = express();
 
 const port = process.env.PORT;
 
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
 app.use(helmet());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', router);
 
